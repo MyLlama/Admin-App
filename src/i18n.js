1d0a23bfe61781e4ed8bad52d688eca94f4a7d60
Aish <aish@myllama.co>
2023-10-23 13:29:12 +0530

    Added support for localization

---
+++ src/i18n.js
@@ -0,0 +1,13 @@
+import { createI18n } from 'vue-i18n'
+
+const messages = {
+  en: {
+  }
+}
+
+const i18n = createI18n({
+  locale: 'en', // Default locale
+  messages,
+})
+
+export default i18n
--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
+import i18n from './i18n'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(i18n)
 
 app.mount('#app')
